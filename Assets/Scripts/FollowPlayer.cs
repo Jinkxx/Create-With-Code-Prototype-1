0632dba995aa7d197340a4569217b44ce9e2d517
Eir <everythinginreturninfo.com>
2025-11-19 17:23:44 -0500

    Followplayer script for camera, fixed camera angles to better show vehicle, added speed variable

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    public float speed = 25.0f;
+public class FollowPlayer : MonoBehaviour
+{   
+    public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +12,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + new Vector3(0, 5, -7);
     }
 }
