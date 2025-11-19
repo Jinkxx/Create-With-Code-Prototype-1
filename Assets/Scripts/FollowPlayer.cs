a4a87ebcc0d5277cdaf227a44ad32098e196da58
Eir <everythinginreturninfo.com>
2025-11-19 17:32:02 -0500

    LateUpdate use in followplayer to fix jittering camera problems, variables for followplayer

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {   
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is used here to get rid of the jittering camera.
+    void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
+        transform.position = player.transform.position + offset;
     }
 }
